442f3a45f98ae6b163f9e9deb350d12667c75b96
sweety-wo <sweety.webashlar@gmail.com>
2017-05-20 17:50:34 +0530

    open side panel for update and create modal view

--- src/app/task/task.controller.js
+++ src/app/task/task.controller.js
@@ -6,14 +6,19 @@
     .controller('TaskController', TaskController);
 
   /** @ngInject */
-  function TaskController(taskObj,$mdDialog) {
+  function TaskController(taskObj, $mdDialog, $mdSidenav) {
     var vm = this;
     vm.taskObj = taskObj;
 
     vm.selectedRecommendation = vm.taskObj.recommendations[0];
 
-    vm.fnCloseDialog = function(){
+    vm.fnCloseDialog = function () {
       $mdDialog.cancel();
+    };
+
+    vm.fnOpenSideNav = function (launchItem) {
+      $mdSidenav('rightView').toggle();
+      vm.selectedItem = launchItem;
     }
 
   }
